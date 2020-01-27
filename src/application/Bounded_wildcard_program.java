620a4d2c044dac18cc5e59f8cc30288f39fa002f
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-01-27 03:38:02 +0000

    Wildcar with get and put

--- src/application/Bounded_wildcard_program.java
+++ src/application/Bounded_wildcard_program.java
@@ -9,7 +9,6 @@ import entities.Shape;
 
 public class Bounded_wildcard_program {
 
-	
 	public static double totalArea(List<? extends Shape> list) {
 		double sum = 0.0;
 		for (Shape s : list) {
@@ -18,21 +17,18 @@ public class Bounded_wildcard_program {
 		return sum;
 	}
 
-	
 	public static void main(String[] args) {
 
 		List<Shape> myShapes = new ArrayList<>();
 		myShapes.add(new Rectangle(3.0, 2.0));
 		myShapes.add(new Circle(2.0));
-		
+
 		List<Circle> myCircle = new ArrayList<>();
 		myCircle.add(new Circle(2.0));
 		myCircle.add(new Circle(3.0));
-		
-
 
 		System.out.println("Total area: " + totalArea(myShapes));
-		
+
 		System.out.println("Total circle area: " + totalArea(myCircle));
 	}
 
